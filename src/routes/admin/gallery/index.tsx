9047ca59e38fb2c75e9d77923f30c708e7a1a5b6
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2026-02-04 23:34:26 +0800

    feat: add loader to gallery route for fetching crochet data

--- src/routes/admin/gallery/index.tsx
+++ src/routes/admin/gallery/index.tsx
@@ -1,6 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
 import GalleryManagement from "@/features/admin/gallery/components/gallery-page/GalleryManagement";
+import { adminDashboardQueryOptions } from "@/features/admin/options";
 
 export const Route = createFileRoute("/admin/gallery/")({
 	component: GalleryManagement,
+	loader: async ({ context }) => {
+		await context.queryClient.ensureQueryData(
+			adminDashboardQueryOptions.getAllCrochets,
+		);
+	},
 });
